42ffd6c3f521586ec77780161929c4fb803236da
Hifzu04 <hifzurbtech@gmail.com>
2024-04-04 17:15:15 +0530

    init

---
+++ pokemon.js
@@ -0,0 +1,8 @@
+const pokemon = https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/1.png;
+
+
+const container = document.querySelector('div');
+const image = document.createElement('img');
+image.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png";
+document.body.appendChild(image);
+
