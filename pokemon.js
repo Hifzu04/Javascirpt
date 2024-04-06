e40208037b097864322cc6d24ad98260d873ed50
Hifzu04 <hifzurbtech@gmail.com>
2024-04-06 16:21:11 +0530

    pgs

--- pokemon.js
+++ pokemon.js
@@ -1,8 +1,12 @@
-const pokemon = https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/1.png;
+//const pokemon = https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/1.png;
 
 
-const container = document.querySelector('div');
-const image = document.createElement('img');
-image.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png";
-document.body.appendChild(image);
+
+const container = document.querySelector('#container');
+for(let i = 1; i< 150 ; i++){
+    
+    const image = document.createElement('img');
+    image.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
+    container.appendChild(image);
+}
 
